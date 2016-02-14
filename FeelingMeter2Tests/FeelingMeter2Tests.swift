a14d543fc6a575e2b904a23f0bfa85433ac4ab31
野中哲 <n757uf@ezweb.ne.jp>
2016-02-14 09:25:51 +0900

    Initial Commit

---
+++ FeelingMeter2Tests/FeelingMeter2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FeelingMeter2Tests.swift
+//  FeelingMeter2Tests
+//
+//  Created by 野中 哲 on 2016/02/14.
+//  Copyright © 2016年 TrueLogic. All rights reserved.
+//
+
+import XCTest
+@testable import FeelingMeter2
+
+class FeelingMeter2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
