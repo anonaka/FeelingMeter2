4407d50613ca08a09efd5eb58141c5127f74e27a
野中哲 <n757uf@ezweb.ne.jp>
2016-03-15 15:28:33 +0900

    dialogs for delete data.

--- FeelingMeter2/FmManagementViewController.swift
+++ FeelingMeter2/FmManagementViewController.swift
@@ -16,12 +16,31 @@ class FmManagementViewController: UIViewController {
     @IBOutlet weak var deleteAllButton: UIButton!
     
     @IBAction func deleteLastOne(sender: AnyObject) {
-        print("delete one")
         fmModel.deleteLastFeelingData()
+        let alertController = UIAlertController(
+            title: NSLocalizedString("Deleted",comment: "dialog title"),
+            message: NSLocalizedString("One data deleted",comment:"one data deleted confirmation"),
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(defaultAction)
+        presentViewController(alertController, animated: true, completion: nil)
     }
     
     @IBAction func deleteAll(sender: AnyObject) {
-        print("delete all")
-        fmModel.deleteAllFeelingData()
+        let alertController = UIAlertController(
+            title: NSLocalizedString("Delete all?", comment: "dialog title"),
+            message: NSLocalizedString("all the data will be deleteed",comment:"dialog message"),
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel",comment:"Cancel button"), style: .Default, handler: nil)
+        
+        let okAction = UIAlertAction(title: NSLocalizedString("OK",comment:"OK"), style: .Default) {
+            action in self.fmModel.deleteAllFeelingData()
+        }
+
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        presentViewController(alertController, animated: true, completion: nil)
     }
 }
